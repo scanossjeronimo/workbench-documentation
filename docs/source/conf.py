2832e42c60138fc7b4aade7798963ef2e692eb9e
Norcal <jeronimoortizlr@gmail.com>
2024-06-06 14:41:04 -0300

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,5 +24,5 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_logo = 'scanoss.png'
+html_logo = 'scanosslogo.png'
 html_static_path = ['_static']
