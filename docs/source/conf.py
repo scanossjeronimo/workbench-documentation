4b356e186979c12088f53873a594be5b214c3a11
Norcal <jeronimoortizlr@gmail.com>
2024-06-06 14:29:40 -0300

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'ScanOSS'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'furo']
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
